3b9ac13ba8aadcf2292946100bd0b66fe85d4da6
haiderali077 <haiderali077@users.noreply.github.com>
2025-06-08 14:38:25 -0700

    updated cors

--- backend/email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorController.java
+++ backend/email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://ai-smart-email-assistant.netlify.app")
 
 public class EmailGeneratorController {
 
